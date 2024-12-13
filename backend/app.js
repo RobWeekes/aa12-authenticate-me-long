545d8ee686f03a80e7c20fcb85a68e69c0d75d80
Robert Weekes <weekes.robert@gmail.com>
2024-12-13 15:05:20 -0800

    comment

--- backend/app.js
+++ backend/app.js
@@ -49,8 +49,6 @@ app.use(
 
 app.use(routes); // Connect all the routes
 
-// backend/app.js
-// ...
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
     const err = new Error("The requested resource couldn't be found.");
