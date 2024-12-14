e894f94b37ee99caf81524acc06d7b92bbd86bdb
Robert Weekes <weekes.robert@gmail.com>
2024-12-13 20:35:09 -0800

    starting out phase 4

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,12 +3,25 @@ const router = require('express').Router();
 
 // backend/routes/api/index.js
 // Import the restoreUser middleware & connect it to the router before any other middleware or route handlers are connected to the router.
-const { restoreUser } = require('../../utils/auth.js');
-router.use(restoreUser);
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
+
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
 
 
 
