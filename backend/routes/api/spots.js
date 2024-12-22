ba7b94cb48288fa168141a728ca13de443a7d42f
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2024-12-21 16:56:37 -0800

    created spot.js model file, migration file and seeder file

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -6,13 +6,34 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const { Spot } = require('../../db/models');
+const { QueryInterface } = require('sequelize');
 
 const router = express.Router();
 
-// develop spot routes here
+// spot paths start with '/spots' (handled by router in index.js)
+
+        // users as template:
+        // const { email, password, username, firstName, lastName } = req.body;
+        // const hashedPassword = bcrypt.hashSync(password);
+        // const user = await User.create({ email, username, hashedPassword, firstName, lastName});
+
+        // const safeUser = {
+        //   id: user.id,
+        //   firstName: user.firstName,
+        //   lastName: user.lastName,
+        //   email: user.email,
+        //   username: user.username,
+        // };
 
 
+
+// Get all Spots
+router.get('/',
+    async (req, res) => {
+    const allSpots = await Spot.findAll()
+    return res.json(allSpots);
+})
 
 
 
