e8c053590c5953b131e562e85ec34fc2627a7fa1
andrea17463 <andrea174633@gmail.com>
2025-01-03 20:43:59 -0800

    updated review-images api routes

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -6,11 +6,16 @@ const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
 router.delete('/:imageId', requireAuth, async (req, res) => {
+  
   const reviewImage = await ReviewImage.findByPk(req.params.imageId, {
-    include: [{ model: Review,
-      attributes: ['id', 'userId']
+    include: [{ 
+      model: Review,
+      attributes: ['userId'] 
     }]
   });
+
+  console.log('Found review image:', reviewImage);
+  console.log('Line 14 - Delete route hit with imageId:', req.params.imageId);
 
   if (!reviewImage) {
     return res.status(404).json({
