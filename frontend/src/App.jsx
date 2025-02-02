f808e01121b4988a8c179dd2b9dc4786299fac38
andrea17463 <andrea174633@gmail.com>
2025-02-01 16:19:40 -0800

    completed phase 1 - LoginFormPage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,25 @@
+// frontend/src/App.jsx
+// added below for phase 1 of frontend readme
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Welcome!</h1>
+  },
+  {
+    path: '/login',
+    element: <LoginFormPage />
+  }
+]);
+// 
+
 function App() {
-  return <h1> Hello from App </h1>;
+  // changed below for phase 1 of frontend readme
+  // return <h1> Hello from App </h1>;
+  return <RouterProvider router={router} />;
+  // 
 }
 
 export default App;
---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,6 @@
+// added below for phase 1 of frontend readme
+// import { LoginFormPage } from './LoginFormPage';
+import LoginFormPage from './LoginFormPage';
+
+export default LoginFormPage;
+//
