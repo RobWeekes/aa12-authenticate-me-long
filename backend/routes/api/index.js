f2b3879d2c55d03edbcaec31b625811bb9d118f8
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2025-02-14 21:02:44 -0800

    commented in backend test index.js route POST /api/test - ran fetch command to test new csrfFetch function at the end of phase 0 - commented test route back out

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -28,21 +28,29 @@ router.use('/review-images', reviewImagesRouter);
 router.use('/bookings', bookingsRouter);
 router.use('/spot-images', spotImagesRouter);
 
-// // Will use this test route later for front end testing
-// router.post('/test', (req, res) => {
-//   res.json({ requestBody: req.body });
-// });
-
 
 
 /////////////////////////////////////////////////
 //// CODE FOR TESTING USER AUTH ROUTES BELOW ////
 
-// // Test this setup by creating the following test route in the api router
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
+
+// // TESTING THE CSRF FUNCTION
+// // comment this test route in:
+
+// // POST /api/test
+// router.post('/test', (req, res) => {
+//   res.json({ requestBody: req.body });
 // });
 
+// // To test the custom csrfFetch function that attaches the CSRF token to the header, navigate to root of app, http://localhost:5173
+// // In the browser console, make a (FETCH) request to POST /api/test with the demo user credentials using the window.csrfFetch function:
+
+// /*
+// csrfFetch('/api/test', {
+//   method: 'POST',
+//   body: JSON.stringify({ credential: 'Demo-lition', password: 'password' })
+// }).then(res => res.json()).then(data => console.log(data));
+// */
 
 // // GET /api/set-token-cookie
 // const { setTokenCookie } = require('../../utils/auth.js');
@@ -80,19 +88,23 @@ router.use('/spot-images', spotImagesRouter);
 //// CODE FOR TESTING USER AUTH ROUTES ABOVE ////
 /////////////////////////////////////////////////
 
+
+
+// NOTE - this POST /api/test route referenced in phase 0 was done above in the backend project ^
+
 // removed the POST /api/test test route below for phase 0 of frontend readme
 // // note: the readme didn't instruct to paste the following code. added below for phase 0 of frontend readme
 // router.post('/test', (req, res) => {
 //     // note: added this because there wasn't an object with a key of requestBody logged in the terminal with the value as the object that was passed into the body of the request. added below for phase 0 of frontend readme
 //     console.log('requestBody:', req.body);
-//     // 
+//     //
 //     const { credential, password } = req.body;
 //     // note: changed this because there wasn't an object with a key of requestBody logged in the terminal with the value as the object that was passed into the body of the request. added below for phase 0 of frontend readme
 //     // res.json({ credential, password });
 //     res.json({ message: "Test endpoint successful",
 //         receivedData: { credential, password } });
-//         // 
+//         //
 // });
-// // 
+// //
 
 module.exports = router;
