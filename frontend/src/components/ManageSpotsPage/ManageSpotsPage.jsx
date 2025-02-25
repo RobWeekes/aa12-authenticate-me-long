bf2d9478fc9b7e184af6955fcf8cb82c868a627c
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2025-02-24 20:09:38 -0800

    use fetchAllSpots instead of fetchSpotById in ManageSpotsPage

--- frontend/src/components/ManageSpotsPage/ManageSpotsPage.jsx
+++ frontend/src/components/ManageSpotsPage/ManageSpotsPage.jsx
@@ -1,15 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { fetchSpotById, deleteSpot } from "../../store/spots";  // Correct import
+// import { fetchSpotById, deleteSpot } from "../../store/spots";
+// use fetchAllSpots instead of fetchSpotById
+import { fetchAllSpots, deleteSpot } from "../../store/spots";
 
 const ManageSpotsPage = () => {
   const dispatch = useDispatch();
   const spots = useSelector((state) => state.spots.spots);
-
+  // useEffect(() => {
+  //   dispatch(fetchSpotById());
+  // }, [dispatch]);
+  // ManageSpotsPage is calling fetchSpotById() without an ID parameter, which would trigger that undefined ID request. ManageSpotsPage should use fetchAllSpots instead since it needs to display all spots managed by the user:
   useEffect(() => {
-    dispatch(fetchSpotById());
+    dispatch(fetchAllSpots());
   }, [dispatch]);
-
+  // This will resolve the undefined spot ID request we're seeing in the logs.
   const handleDelete = (id) => {
     dispatch(deleteSpot(id));
   };
